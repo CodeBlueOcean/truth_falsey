2f8842848bd7a5bef5e7f8b37af5129d92764c3f
CodeBlueOcean <95150255+CodeBlueOcean@users.noreply.github.com>
2022-03-08 14:12:42 -0600

    Add files via upload

---
+++ truthy-falsey.py
@@ -0,0 +1,40 @@
+# another example
+is_old = bool('hello')
+is_licenced = bool(5)
+
+print(bool(''))
+print(bool(0))
+
+if is_old and is_licenced:
+    print('you are old enough to drive, and you have a licence!')
+else:
+    print('you are not of age!')
+
+print('okoko')
+
+# another example
+is_old = bool('hello')
+is_licenced = bool(5)
+
+print(bool('hello'))
+print(bool(5))
+
+if is_old and is_licenced:
+    print('you are old enough to drive, and you have a licence!')
+else:
+    print('you are not of age!')
+
+print('okoko')
+
+# another example
+password = '123'
+username = 'johnny'
+
+# Truthy and Falsy
+
+if password and username:
+    print('you are old enough to drive, and you have a licence!')
+else:
+    print('you are not of age!')
+
+print('okoko')
